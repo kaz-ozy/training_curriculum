21aadb85445cafae0d0dc0dfbf92cbecfc3eae5b
kaz-ozy <132529533+kaz-ozy@users.noreply.github.com>
2023-05-23 22:20:39 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -21,12 +21,14 @@ class CalendarsController < ApplicationController
 
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
+    wdays[Date.today.wday]
+    
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
+    
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
@@ -35,9 +37,30 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      wday_num =  Date.today.wday + x
+      if  wday_num >= 7
+        wday_num = wday_num - 7
+      end
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}
       @week_days.push(days)
     end
-
   end
 end
+
+# いまほしいあたい
+# 2,3,4,5,6,
+
+# 2 = 2 + 0
+# 3 = 2 + 1
+# 4 = 2 + 2
+
+# Date.today.wday = 2
+# より、
+# 2 = Date.today.wday + 0
+# 3 = Date.today.wday + 1
+# 4 = Date.today.wday + 2
+
+# wdays[7]は配列にはよういされていない
+# wdays[0]なら日曜日が表示される
